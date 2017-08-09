6517ffe1d1995e8c8227f04189ab87d018ede44e
asedghi <asedghi@sfu.ca>
2017-08-09 13:34:50 -0700

    Created to-do list app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: [
+        {id: 1, name:'Learn JSX', isComplete:true},
+        {id: 2, name:'Learn React', isComplete:false},
+        {id: 3, name:'Learn BS', isComplete:false}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Amid's Github</h2>
+          <h2>
+            <b>Welcome to Amid's Github</b>
+            </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className ="Todo-App">
+          <form> 
+            <input type = "text"/> 
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.state.todos.map( todo => 
+              <li key = {todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+              </li>)}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* The root element is coming from the index.html
+*/
+ReactDOM.render(<App txt="this is a prop"/>, document.getElementById('root'));
 registerServiceWorker();
