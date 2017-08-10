289600d605053316ab308d87df7042976f0297af
asedghi <asedghi@sfu.ca>
2017-08-10 11:19:42 -0700

    Items now addable to the list

--- src/App.js
+++ src/App.js
@@ -1,62 +1,54 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-var TodoItems = React.createClass({
-  render: function() {
-    var todoEntries = this.props.entries;
- 
-    function createTasks(item) {
-      return <li key={item.id}>{item.name}</li>
-    }
- 
-    var listItems = todoEntries.map(createTasks);
- 
-    return (
-      <ul className="theList">
-        {listItems}
-      </ul>
-    );
-  }
-});
-
 class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
-      todos: [
-        {id: 1, name:'Learn JSX', isComplete:true},
-        {id: 2, name:'Learn React', isComplete:false},
-        {id: 3, name:'Learn BS', isComplete:false}
-      ]
+      items: [
+        //{key: 1, text:'Learn JSX', isComplete:true},
+        //{key: 2, text:'Learn React', isComplete:false},
+        //{key: 3, text:'Learn BS', isComplete:false}
+      ],
+      date: new Date() 
     }
   }
 
-  addItem(e){
-    var itemArray = this.state.todos; 
+  /*
+  getInitialState(){
+    return {
+      items: []
+    }
+  }
+  */
 
-    this.state.todos.push(
+  addItem = (event) => {
+    this.state.items.push(
       {
-        id: Date.now(), 
-        name: this._inputElement.value, 
-        isComplete: false
-      } 
+        key: Date.now(),
+        text: this._inputElement.value,
+        isComplete:false
+      }
     );
 
-    /* this.setState({
-      todos: itemArray
-    }); */
-
+    console.log(this.state.items); 
+    // reset the box to nothing after
     this._inputElement.value = "";
 
-    /* Add button will trigger our browser's default POST behavior 
-      - which we definitely don't want.
-    */
-    e.preventDefault(); 
+    event.preventDefault(); 
+
   }
 
   render() {
+    /*
+    var todoEntries = this.props.entries; 
+    function createTasks(item){
+      return <li key={item.key}>{item.text}</li>
+    };
+    var listItems = todoEntries.map(createTasks);
+    */
     return (
       <div className="App">
         <div className="App-header">
@@ -64,22 +56,21 @@ class App extends Component {
           <h2>
             <b>Welcome to Amid's Github</b>
           </h2>
-          <h3>It is {new Date().toLocaleTimeString()} PDT</h3>
+          <h3>It is {this.state.date.toLocaleTimeString()} PDT</h3>
         </div>
         <div className ="Todo-App">
-          <form onSubmit={this.addItem}> 
-            <input ref={(a) => this._inputElement = a}
-              type = "text" placeholder="Enter Task">
+          <form onSubmit= {this.addItem}>
+            <input placeholder = "Enter Task" ref = { (a) => 
+                this._inputElement = a}>
             </input> 
             <button type = "submit">Add</button> 
           </form>
-          <TodoItems entries={this.state.todos}/>
           <div className="Todo-List">
             <ul>
-              {this.state.todos.map( todo => 
-              <li key = {todo.id}>
-                <input type="checkbox" defaultChecked={todo.isComplete}/> 
-                {todo.name}
+              {this.state.items.map( item => 
+              <li key = {item.key}>
+                <input type="checkbox" defaultChecked={item.isComplete}/> 
+                {item.text}
               </li>)}
             </ul>
           </div>
